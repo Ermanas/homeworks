2fe19ec612757d80e0908f84d15af3e102f19194
Ermanas <ermanasyabanci@gmail.com>
2024-01-11 23:56:32 +0300

    add: doxygen style comments to string_trim library

--- homeworks/homework_4/no_strings_attached/no_strings_attached/string_trim.h
+++ homeworks/homework_4/no_strings_attached/no_strings_attached/string_trim.h
@@ -12,7 +12,22 @@ namespace no_strings_attached
         kBoth
     };
 
+/**
+ * @brief Trims a given string from sides with a specified char and direction
+ * 
+ * @param str The input string to be trimmed
+ * @param char_to_trim The character to use to trim
+ * @param side The side of the trim from left, right or both.
+ * @return std::string Returns the trimmed string as string.
+ */
     std::string Trim(const std::string &str, char char_to_trim, Side side);
+
+/**
+ * @brief Trims a given string's spaces from both sides
+ * 
+ * @param str The input string to be trimmed
+ * @return std::string Returns the trimmed string as string.
+ */
     std::string Trim(const std::string& str);
 } // no_strings_attached
 #endif
